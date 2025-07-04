350dfa8448f7533d35b1c2ab6586f6aed8366d08
daniel <daniel.rodinger@protonmail.com>
2025-07-04 23:10:51 +0200

    add ResNet to models import

--- train_crossval.py
+++ train_crossval.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 import sys
 from functools import partial
 
-from models.model_classifier import AudioMLP, SimpleResNet
+from models.model_classifier import AudioMLP, SimpleResNet, ResNet
 from models.utils import EarlyStopping, Tee
 from dataset.dataset_ESC50 import ESC50
 import config
